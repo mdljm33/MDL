2d2f1036a801e97e7f57b4dca7048eeecbbba7a4
mdljm33 <mdljeanmoulinlangon@gmail.com>
2025-03-10 16:44:14 +0100

    essa

--- QRcode01/index.php
+++ QRcode01/index.php
@@ -4,17 +4,17 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/QRcode01/style/styles.css">
+  <link rel="stylesheet" href="../QRcode01/style/styles.css">
   <title>Chasse aux QR Codes</title>
 </head>
 
 <header>
-  <?php include './header.php'; ?>
+  <?php include '../QRcode01/Modules/header.php'; ?>
 </header>
 
 <body>
 <body>
 
 <footer>
-    <?php include './Modules/footer.php'; ?>
+    <?php include '../QRcode01/Modules/footer.php'; ?>
 <footer>
