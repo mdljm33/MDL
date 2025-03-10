fd1639f99dbdeedc940322d8701595b330861a5d
mdljm33 <mdljeanmoulinlangon@gmail.com>
2025-03-10 16:32:45 +0100

    chgt qr code 01

--- QRcode01/index.php
+++ QRcode01/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/QRcode/style/styles.css">
+  <link rel="stylesheet" href="/QRcode01/style/styles.css">
   <title>Chasse aux QR Codes</title>
 </head>
 
