51c2ebbd9b36636e15b81f8caa2cede48e973afa
mdljm33 <mdljeanmoulinlangon@gmail.com>
2025-03-17 14:54:13 +0100

    finish inscription

--- QRcode01/Modules/register_user.php
+++ QRcode01/Modules/register_user.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
  $sql = "INSERT INTO user (nom, prenom, classe, email, password) VALUES ('$nom','$prenom','$classe', '$email', '$password')";
  
         if ($conn->query($sql) === TRUE) {
-            echo "Inscription réussie. Vous pouvez vous connecter !";
+            header('Location:/MDL/Qrcode01/index.php');
         } else {
             echo "Erreur : " . $conn->error;
          }
