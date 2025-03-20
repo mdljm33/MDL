ebeb67a580173aacf7c7b3997278c24b4aa35b6d
mdljm33 <mdljeanmoulinlangon@gmail.com>
2025-03-20 16:25:19 +0100

    classement avec position

--- QRcode01/Pages/Classement.php
+++ QRcode01/Pages/Classement.php
@@ -18,12 +18,9 @@
 <div class="client-area">
     <div class="container">
       <h2>CLASSEMENT<h2>
-    <?php
-// Inclure le fichier de connexion à la base de données
-include '../Modules/bd.php';
-
-// Requête SQL pour récupérer les informations triées par score (du plus petit au plus grand)
-$sql = "SELECT nom, prenom, classe, score FROM user ORDER BY score DESC "; // ASC pour tri croissant
+      <?php
+// Requête SQL pour récupérer les informations triées par score (du plus grand au plus petit)
+$sql = "SELECT nom, prenom, classe, score FROM user ORDER BY score DESC";
 $score = $conn->query($sql);
 
 // Vérifie s'il y a des résultats
@@ -31,20 +28,28 @@ if ($score->num_rows > 0) {
     // Si des résultats sont trouvés, affiche les données dans un tableau
     echo "<table border='1' cellspacing='0' cellpadding='10'>
             <tr>
+                <th>Classement</th>
                 <th>Nom</th>
                 <th>Prénom</th>
                 <th>Classe</th>
                 <th>Score</th>
             </tr>";
+            
+            $ranking_position = 1;  // Initialiser le compteur de classement
 
     // Affiche chaque ligne de résultat
     while($row = $score->fetch_assoc()) {
+
+
         echo "<tr>
+                <td>" . $ranking_position . "e</td>  <!-- Affichage du classement -->
                 <td>" . $row["nom"] . "</td>
                 <td>" . $row["prenom"] . "</td>
                 <td>" . $row["classe"] . "</td>
                 <td>" . $row["score"] . "</td>
               </tr>";
+
+        $ranking_position++;  // Incrémenter la position du classement
     }
 
     echo "</table>";  // Fermer la table HTML
@@ -55,6 +60,7 @@ if ($score->num_rows > 0) {
 // Ferme la connexion à la base de données
 $conn->close();
 ?>
+
     </div>
   </div>
 </body>
